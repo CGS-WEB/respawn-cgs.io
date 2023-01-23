db1b175a8f6fefe1b84917b21cbb08fac1c59e42
AlexSlayer69 <akulvjha2808@gmail.com>
2023-01-23 20:27:31 +0530

    Initial Changes
    Changed the page breaking from 3 to 2 and added a box to the circles

---
+++ www.translate-wf.com/circles.js
@@ -0,0 +1,26 @@
+document.getElementsByClassName("sw2")[0].getElementsByClassName("webis-ill-circle")[0].addEventListener("click", f_0());
+document.getElementsByClassName("sw2")[0].getElementsByClassName("webis-ill-circle")[1].addEventListener("click", f_1());
+document.getElementsByClassName("sw2")[0].getElementsByClassName("webis-ill-circle")[2].addEventListener("click", f_2());
+document.getElementsByClassName("sw2")[0].getElementsByClassName("webis-ill-circle")[3].addEventListener("click", f_3());
+
+function f_0() {
+    document.getElementById("webis-box-text").classList = "webis-box";
+    document.getElementById("webis-box-text").innerHTML = "Text_1";
+    console.log("haaaaaaa");
+}
+function f_1() {
+    document.getElementById("webis-box-text").classList = "webis-box";
+    document.getElementById("webis-box-text").innerHTML = "Text_2";
+}
+function f_2() {
+    document.getElementById("webis-box-text").classList = "webis-box";
+    document.getElementById("webis-box-text").innerHTML = "Text_3";
+}
+function f_3() {
+    document.getElementById("webis-box-text").classList = "webis-box";
+    document.getElementById("webis-box-text").innerHTML = "Text_4";
+}
+
+function f_4() {
+    document.getElementById("webis-box-text").classList = "webis-non";
+}
