728b356745e85c644cd3eb1c12a378e5e728ace9
AlexSlayer69 <akulvjha2808@gmail.com>
2023-01-28 05:50:50 +0530

    Boxes

--- www.translate-wf.com/circles.js
+++ www.translate-wf.com/circles.js
@@ -1,26 +1,25 @@
-document.getElementsByClassName("sw2")[0].getElementsByClassName("webis-ill-circle")[0].addEventListener("click", f_0());
-document.getElementsByClassName("sw2")[0].getElementsByClassName("webis-ill-circle")[1].addEventListener("click", f_1());
-document.getElementsByClassName("sw2")[0].getElementsByClassName("webis-ill-circle")[2].addEventListener("click", f_2());
-document.getElementsByClassName("sw2")[0].getElementsByClassName("webis-ill-circle")[3].addEventListener("click", f_3());
+document.querySelectorAll("#circles-wrapper .circles")[0].addEventListener("click", f_0());
+document.querySelectorAll("#circles-wrapper .circles")[1].addEventListener("click", f_0());
+document.querySelectorAll("#circles-wrapper .circles")[2].addEventListener("click", f_0());
+document.querySelectorAll("#circles-wrapper .circles")[3].addEventListener("click", f_0());
 
 function f_0() {
-    document.getElementById("webis-box-text").classList = "webis-box";
-    document.getElementById("webis-box-text").innerHTML = "Text_1";
-    console.log("haaaaaaa");
+    document.getElementById("circles-box").classList = "";
+    document.getElementById("circles-box").innerHTML = "Text_1";
 }
 function f_1() {
-    document.getElementById("webis-box-text").classList = "webis-box";
-    document.getElementById("webis-box-text").innerHTML = "Text_2";
+    document.getElementById("circles-box").classList = "";
+    document.getElementById("circles-box").innerHTML = "Text_2";
 }
 function f_2() {
-    document.getElementById("webis-box-text").classList = "webis-box";
-    document.getElementById("webis-box-text").innerHTML = "Text_3";
+    document.getElementById("circles-box").classList = "";
+    document.getElementById("circles-box").innerHTML = "Text_3";
 }
 function f_3() {
-    document.getElementById("webis-box-text").classList = "webis-box";
-    document.getElementById("webis-box-text").innerHTML = "Text_4";
+    document.getElementById("circles-box").classList = "";
+    document.getElementById("circles-box").innerHTML = "Text_4";
 }
 
 function f_4() {
-    document.getElementById("webis-box-text").classList = "webis-non";
+    document.getElementById("circles-box").classList = "webis-non";
 }
